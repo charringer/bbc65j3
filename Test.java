91edf7bca95bb283868975ca2857ff39568440c3
Claudio Harringer <der.claudio@gmail.com>
2010-12-15 16:11:48 +0100

    Add framework: unit test lib, ant-file, gitignore.

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		TestRunner.main(
+				BluRayDriveTest.class,
+				CardReaderTest.class,
+				PortTest.class,
+				USBHardDiskAdapterTest.class,
+				RootTest.class);
+	}
+
+}
