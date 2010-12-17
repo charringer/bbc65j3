a1c908b09f498e3286eae8e4ab12b6168d8fcdcb
Gabriel Ebner <ge@gabrielebner.at>
2010-12-17 22:20:04 +0100

    Fix build.

--- Test.java
+++ Test.java
@@ -2,12 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		TestRunner.main(
-				BluRayDriveTest.class,
-				CardReaderTest.class,
-				PortTest.class,
-				USBHardDiskAdapterTest.class,
-				RootTest.class);
+		TestRunner.main();
 	}
 
 }
