fd4aa1923f147592958c6c29186d14f977444712
hanse <thehanse@gmail.com>
2010-12-21 19:10:09 +0100

    Äh Zusicherungen hier

--- Player.java
+++ Player.java
@@ -3,12 +3,17 @@ public abstract class Player extends Thread {
 	private long sleepTime;
 	private boolean dead;
 
+	/* saves startCell and sleepTime 
+	 *  startCell is place where the Player starts
+	 *  sleepTime time the player sleeps between each move
+	 */
 	public Player(Cell startCell, long sleepTime) {
 		currentCell = startCell;
 		this.sleepTime = sleepTime;
 		this.dead = false;
 	}
 
+	/* calles step() every sleepTime milliseconds */
 	public void run() {
 		while (true) {
 			try {
@@ -21,10 +26,13 @@ public abstract class Player extends Thread {
 		}
 	}
 
+	/* stops the current player
+	 * by interrupting the thread */
 	public void kill() {
 		this.dead = true;
 		this.interrupt();
 	}
 
+	/* should move the player */
 	protected abstract void step();
 }
